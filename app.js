d99b72be7d96edbac1b64649b6bd1b44fde45506
KRITI KUMARI <kriti.kumari0718@gmail.com>
2026-09-21 23:53:38 +0530

    Add getUser and divide functions to app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+   function getUser(db, id) {
+     return db.query("SELECT * FROM users WHERE id = " + id);
+   }
+
+   function divide(a, b) {
+     return a / b;
+   }
+
+   module.exports = { getUser, divide };
